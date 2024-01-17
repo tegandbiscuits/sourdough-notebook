ae5e88147aac580632bc067c316ee80fc029f0fc
Tegan Rauh <3896310+tegandbiscuits@users.noreply.github.com>
2024-01-17 17:43:35 -0500

    Add daisyui

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <a
-          className="App-link"
+          className="btn btn-primary"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
