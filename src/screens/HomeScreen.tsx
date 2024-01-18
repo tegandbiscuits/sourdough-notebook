8580641aa9078b02676a27d187c02609b67abd2a
Tegan Rauh <3896310+tegandbiscuits@users.noreply.github.com>
2024-01-18 01:06:49 -0500

    Scaffold totals card

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from '../components/Card';
 import NumberField from '../components/NumberField';
+import KeyValueList from '../components/KeyValueList';
 
 function HomeScreen() {
   return (
@@ -45,19 +46,25 @@ function HomeScreen() {
                 unit: 'gram',
               }}
             />
-
-            <NumberField
-              label="Salt (grams)"
-              placeholder="Starter"
-              isReadOnly
-              value={10}
-              minValue={0}
-              formatOptions={{
-                style: 'unit',
-                unit: 'gram',
-              }}
-            />
           </form>
+        </Card.Body>
+      </Card>
+
+      <Card className="mt-8" variant="info">
+        <Card.Body>
+          <Card.Title>
+            Totals
+          </Card.Title>
+
+          <KeyValueList>
+            <KeyValueList.Item title="Hydration:" value="100g" />
+            <KeyValueList.Item title="Flour:" value="100g" />
+            <KeyValueList.Item title="Water:" value="100g" />
+            <KeyValueList.Item title="Starter:" value="100g" />
+            <KeyValueList.Item title="Recommended Salt:" value="10g" />
+          </KeyValueList>
+          {/* TODO: note that hydration assumes a 1:1:1 for starter */}
+          {/* TODO: note that hydration assumes a 2% salt to flour */}
         </Card.Body>
       </Card>
     </div>
